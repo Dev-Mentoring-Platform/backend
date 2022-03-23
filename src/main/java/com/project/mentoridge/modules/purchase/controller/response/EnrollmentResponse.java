2608c416db128261603085c65fefd62a4b896600
yeonkyungJoo <younkyong94@gmail.com>
2022-03-24 08:04:04 +0900

    EnrollmentResponse - 강의 등록일자 추가

--- src/main/java/com/project/mentoridge/modules/purchase/controller/response/EnrollmentResponse.java
+++ src/main/java/com/project/mentoridge/modules/purchase/controller/response/EnrollmentResponse.java
@@ -1,6 +1,7 @@
 package com.project.mentoridge.modules.purchase.controller.response;
 
 import com.project.mentoridge.modules.purchase.vo.Enrollment;
+import com.project.mentoridge.utils.LocalDateTimeUtil;
 import lombok.Data;
 
 @Data
@@ -8,11 +9,14 @@ public class EnrollmentResponse {
 
     private String mentee;
     private String lectureTitle;
+    // 강의 등록일자 추가
+    private String createdAt;
 
     // TODO - 쿼리
     // TODO : CHECK - Lecture가 이미 영속성 컨텍스트에 존재
     public EnrollmentResponse(Enrollment enrollment) {
         this.mentee = enrollment.getMentee().getUser().getUsername();
         this.lectureTitle = enrollment.getLecture().getTitle();
+        this.createdAt = LocalDateTimeUtil.getDateTimeToString(enrollment.getCreatedAt());
     }
 }
