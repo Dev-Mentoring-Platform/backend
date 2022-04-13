f911586c4985e6021899f951ac7b47f0234a8ddf
yeonkyungJoo <younkyong94@gmail.com>
2022-04-13 21:48:33 +0900

    Conflict 해결

--- src/main/java/com/project/mentoridge/modules/purchase/controller/response/PickWithSimpleLectureResponse.java
+++ src/main/java/com/project/mentoridge/modules/purchase/controller/response/PickWithSimpleLectureResponse.java
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 package com.project.mentoridge.modules.purchase.controller.response;
 
 import com.project.mentoridge.modules.lecture.controller.response.SimpleLectureResponse;
@@ -16,4 +17,7 @@ public class PickWithSimpleLectureResponse {
 =======
 package com.project.mentoridge.modules.purchase.controller.response;public class PickWithSimpleLectureResponse {
 >>>>>>> e3fd6a89e80deff5eb69b442fb807180fe2f2235
+=======
+package com.project.mentoridge.modules.purchase.controller.response;public class PickWithSimpleLectureResponse {
+>>>>>>> e3fd6a8... pick 리스트 API 수정 및 전체 테스트
 }
--- src/main/java/com/project/mentoridge/modules/purchase/repository/PickQueryRepository.java
+++ src/main/java/com/project/mentoridge/modules/purchase/repository/PickQueryRepository.java
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 package com.project.mentoridge.modules.purchase.repository;
 
 import com.project.mentoridge.modules.account.vo.Mentee;
@@ -48,4 +49,7 @@ public class PickQueryRepository {
 =======
 package com.project.mentoridge.modules.purchase.repository;public class PickQueryRepository {
 >>>>>>> e3fd6a89e80deff5eb69b442fb807180fe2f2235
+=======
+package com.project.mentoridge.modules.purchase.repository;public class PickQueryRepository {
+>>>>>>> e3fd6a8... pick 리스트 API 수정 및 전체 테스트
 }
