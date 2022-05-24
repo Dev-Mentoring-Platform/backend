e59ca0febcdd0cced69d4467cd018d35b3a2bfd6
yeonkyung <younkyong94@gmail.com>
2022-05-24 13:42:39 +0900

    자유게시판 검색 기능

--- src/main/java/com/project/mentoridge/modules/board/controller/request/ContentSearchRequest.java
+++ src/main/java/com/project/mentoridge/modules/board/controller/request/ContentSearchRequest.java
@@ -6,7 +6,7 @@ import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class ConentSearchRequest {
+public class ContentSearchRequest {
 
     private String content;
 }
--- src/main/java/com/project/mentoridge/modules/board/repository/ContentSearchRepository.java
+++ src/main/java/com/project/mentoridge/modules/board/repository/ContentSearchRepository.java
@@ -1,22 +1,25 @@
 package com.project.mentoridge.modules.board.repository;
 
-import com.project.mentoridge.modules.board.controller.request.ConentSearchRequest;
+import com.project.mentoridge.modules.board.controller.request.ContentSearchRequest;
 import com.project.mentoridge.modules.board.controller.response.PostResponse;
+import com.project.mentoridge.modules.board.vo.Comment;
 import com.project.mentoridge.modules.board.vo.Post;
 import com.project.mentoridge.modules.board.vo.QComment;
 import com.project.mentoridge.modules.board.vo.QPost;
-import com.project.mentoridge.modules.lecture.repository.dto.LectureMentorQueryDto;
 import com.querydsl.core.QueryResults;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.hibernate.validator.internal.util.Contracts.assertNotNull;
 
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
@@ -29,26 +32,27 @@ public class ContentSearchRepository {
     private final QComment comment = QComment.comment;
 
     // SELECT * FROM post p INNER JOIN comment c ON p.post_id = c.post_id WHERE p.title LIKE '%content%' OR p.CONTENT LIKE '%content%' OR c.CONTENT LIKE '%content%'
-    public Page<PostResponse> findPostsSearchedByContent(ConentSearchRequest searchRequest, Pageable pageable) {
+    public Page<PostResponse> findPostsSearchedByContent(String content, Pageable pageable) {
 
-        if (searchRequest != null && StringUtils.isNotBlank(searchRequest.getContent())) {
-            String content = searchRequest.getContent();
+        // assertNotNull(searchRequest);
+        // String content = searchRequest.getContent();
 
-            // 각각 조회
-            // post
-            QueryResults<Post> posts = jpaQueryFactory.selectFrom(post)
-                    .offset(pageable.getOffset())
-                    .limit(pageable.getPageSize())
-                    .where(post.title.containsIgnoreCase(content), post.content.containsIgnoreCase(content))
-                    .fetchResults();
+        // 각각 조회
+        // comment
+        List<Comment> comments = jpaQueryFactory.selectFrom(comment)
+                .where(comment.content.containsIgnoreCase(content)).fetchResults().getResults();
+        List<Long> postIds = comments.stream()
+                .map(comment -> comment.getPost().getId()).collect(Collectors.toList());
 
-            // comment
+        // post
+        QueryResults<Post> posts = jpaQueryFactory.selectFrom(post)
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .where(post.title.containsIgnoreCase(content).or(post.content.containsIgnoreCase(content)).or(post.id.in(postIds)))
+                .fetchResults();
 
-        } else {
-
-
-
-        }
-        return null;
+        List<PostResponse> postResponses = posts.getResults().stream()
+                .map(PostResponse::new).collect(Collectors.toList());
+        return new PageImpl<>(postResponses, pageable, posts.getTotal());
     }
 }
