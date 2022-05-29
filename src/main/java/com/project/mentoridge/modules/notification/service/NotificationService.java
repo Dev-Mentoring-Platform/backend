3fb5960558084dda732e33544d72626bdb335cc8
yeonkyungJoo <younkyong94@gmail.com>
2022-05-29 14:03:50 +0900

    notification 소켓 전송 시 id 추가

--- src/main/java/com/project/mentoridge/modules/notification/service/NotificationService.java
+++ src/main/java/com/project/mentoridge/modules/notification/service/NotificationService.java
@@ -10,13 +10,9 @@ import com.project.mentoridge.modules.notification.repository.NotificationReposi
 import com.project.mentoridge.modules.notification.vo.Notification;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
 
 import static com.project.mentoridge.config.exception.EntityNotFoundException.EntityType.NOTIFICATION;
 import static com.project.mentoridge.config.exception.EntityNotFoundException.EntityType.USER;
@@ -54,13 +50,13 @@ public class NotificationService extends AbstractService {
 
     public Notification createNotification(User user, NotificationType type) {
 
-        Notification notification = Notification.builder()
+        // TODO - 비동기로 변환
+        Notification notification = notificationRepository.save(Notification.builder()
                 .user(user)
                 .type(type)
-                .build();
-        // TODO - 비동기로 변환
+                .build());
         messageSendingTemplate.convertAndSend("/sub/notification/" + user.getId(), new NotificationMessage(notification));
-        return notificationRepository.save(notification);
+        return notification;
     }
 
 /*
